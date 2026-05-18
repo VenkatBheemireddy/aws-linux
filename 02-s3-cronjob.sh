de24cc48c921a6aede423b9e4015cb8f7993e5e3
VenkatBheemireddy <venkat.bheemireddy@gmail.com>
2026-05-18 13:08:16 +1000

    Terraform commit

---
+++ 02-s3-cronjob.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws s3 ls >
+
+chmod +x /home/ubuntu/list_s3.sh
+
+
---
+++ 03-aws-resource-tracker.sh
@@ -0,0 +1,16 @@
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# List S3 buckets
+aws s3 ls
+
+# List EC2 instances
+aws ec2 describe-instances
+
+# List Lambda Functions
+aws lambda list-functions
+
+# List IAM Users
+aws iam list-users
